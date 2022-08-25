7bf8ec64dc81b82bdd5ac24f7e67de78eee55e2a
silvioukoth <silvioukoth@gmail.com>
2022-08-25 03:18:44 -0700

    add task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     result = 0
     for i in range(len(sys.argv) - 1):
         result += int(sys.argv[i + 1])
-        print("{}".format(total))
+    print("{}".format(total))
