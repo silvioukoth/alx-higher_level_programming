2961bc2a1f0d774825e1b743de53c19e6b99397d
silvioukoth <silvioukoth@gmail.com>
2022-09-03 03:35:37 -0700

    add task 100

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:c}".format(i - 32) if i % 2 != 0 else "{:c}".format(i), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
+
