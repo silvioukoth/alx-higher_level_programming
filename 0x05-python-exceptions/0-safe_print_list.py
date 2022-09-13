dee4d90ff2abcc3dd623203fa0b8621a79388108
silvioukoth <silvioukoth@gmail.com>
2022-09-13 00:44:33 -0700

    add task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     ret = 0
     for j in range(x):
         try:
-             print("{}".format(my_list[i]), end="")
-             ret += 1
+           print("{}".format(my_list[i]), end="")
+           ret += 1
     except IndexError:
-            break
+           break
     print("")
     return (ret)
