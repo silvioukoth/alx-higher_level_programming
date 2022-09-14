476438ce8b5ef39621caad8fce351bb07f216e15
silvioukoth <silvioukoth@gmail.com>
2022-09-14 06:10:16 -0700

    add task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -8,7 +8,7 @@ class Square:
          """Initialize a new square.
 
          args:
-             size (int): The size of the new square.
+         size (int): The size of the new square.
          """
          self.size = size
 
