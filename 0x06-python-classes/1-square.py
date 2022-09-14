588e28de99ae209e74869bd71ce4ab0389a54774
silvioukoth <silvioukoth@gmail.com>
2022-09-14 04:17:30 -0700

    add task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This Square defines a square by based"""
+
+class Square:
+    """This Square defines a square by based"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        args:
+
+        size (int): The size of the new square.
+        """
+        self.__size = size
