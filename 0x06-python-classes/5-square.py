a01651c05aa4d528ecc825215975415730da40c5
silvioukoth <silvioukoth@gmail.com>
2022-09-14 06:52:33 -0700

    add task 6

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -26,11 +26,11 @@ class Square:
          self.__size = value
 
      def area(self):
-         """Return the current area of the square."""
+          """Return the current area of the square."""
          return (self.__size * self.__size)
 
      def my_print(self):
-         """Print the square with the # character."""
+          """Print the square with the # character."""
          for i in range(0, self.__size):
              [print("#", end="") for j in range(self.__size)]
              print("")
