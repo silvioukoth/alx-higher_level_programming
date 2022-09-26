0899dda07339f90e316d16e2ea7839b018be3ea8
silvioukoth <silvioukoth@gmail.com>
2022-09-26 05:15:06 -0700

    add tasks completed

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the inherited class Mylist."""
+
+class MyList(list):
+    """inherits sorted list."""
+
+def print_sorted(self):
+    """prints the list, but sorted (ascending sort)."""
+    print(sorted(self))
