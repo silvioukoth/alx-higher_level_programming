6b71256921346ed6be42ee97fffff3330351dbf6
silvioukoth <silvioukoth@gmail.com>
2022-09-28 04:10:39 -0700

    add task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -14,11 +14,11 @@ def read_lines(filename="", nb_lines=0):
             print(f.read(), end="")
             return
 
-         lines = 0
-         for line in f:
-             lines += 1
-         f.seek(0)
-         if nb_lines >= lines:
+        lines = 0
+        for line in f:
+            lines += 1
+        f.seek(0)
+        if nb_lines >= lines:
             print(f.read(), end="")
             return
         else:
