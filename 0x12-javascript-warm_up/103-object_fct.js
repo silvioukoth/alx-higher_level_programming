3af8bc639bc6a7b0a27e868724da8107a18df8b8
silvioukoth <silvioukoth@gmail.com>
2022-11-28 05:27:37 -0800

    add advance tasks

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+let myObject = {
+	type: 'object',
+	value: 12
+};
+
+console.log(myObject);
+
+myObject.incr = funnction() {
+	this.valu++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
