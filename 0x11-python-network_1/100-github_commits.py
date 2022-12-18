f90a7234b4265d2d3559836e6d5a10b4dc8195c4
silvioukoth <silvioukoth@gmail.com>
2022-12-17 23:50:24 -0800

    add new updates

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,18 +5,15 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-
-    repos = argv[1]
-    own = argv[2]
-
-    url = 'https://api.github.com/repos/{}/{}/\
-commits?per_page=10'.format(owner, repo)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
     r = requests.get(url)
-
-    r = r.json()
-
-    for commit in r:
-        print('{}: {}'.format(commit.get('sha'),
-                               commit.get('commit').get('author').get('name')))
-
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
