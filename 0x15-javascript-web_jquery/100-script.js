b1e43997ab8934bf21b734cff7d79e051fc021dd
silvioukoth <silvioukoth@gmail.com>
2023-01-25 03:35:13 -0800

    add commits

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContent', function () {
+  document.querySeletor('header').style.color = '#FF0000';
+});
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+This is about 0x15. JavaScript - Web jQuery project.
